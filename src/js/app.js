0271bb301791f553b82183a7e837b0e66681ee63
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-02-28 17:14:55 -0600

    dark mode desde preferencias del sistema

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,15 @@ eventListeners();
 });
 
 function darkMode() {
+
+  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+  prefiereDarkMode.matches? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
+
+  prefiereDarkMode.addEventListener('change', function() {
+    prefiereDarkMode.matches? document.body.classList.add('dark-mode') : document.body.classList.remove('dark-mode')
+  })
+
   const btnDarkMode = document.querySelector('.dark-mode-boton');
   btnDarkMode.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
