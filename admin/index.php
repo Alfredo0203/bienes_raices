b263f5fe635484d4c4e1981029525c8326bd867c
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-03-16 09:45:29 -0600

    Creacion de table para propiedades

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,31 @@ incluirTemplate('header');
         </p>
             <?php } ?>
         <a href="../admin/propiedades/create.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Image</th>
+                    <th>Price</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="../../bienes_raices/src/img/anuncio3.jpg" alt="anuncio" class="img-table"></td>
+                    <td>$20000</td>
+                    <td>
+                        <a href="#" class="boton-amarillo-block">Update</a>
+                        <a href="#" class="boton-rojo-block">Delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
     <?php
