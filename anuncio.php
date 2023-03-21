892e88bcfa0e30a38bf6c56db6e9f9ba5443514d
Alfredo Torres <alfredotorres.niu@gmail.com>
2023-03-20 19:00:08 -0600

    Ver detalles de propiedad desde bd

--- anuncio.php
+++ anuncio.php
@@ -1,52 +1,59 @@
 <?php
 require 'includes/funciones.php';
-
+require 'includes/config/database.php';
 incluirTemplate('header');
+
+$id = $_GET['id'];
+$id = filter_var($id, FILTER_VALIDATE_INT);
+
+
+
+$db = conectarBD();
+
+$consulta = "SELECT * from propiedades WHERE id = {$id}";
+$resultado = mysqli_query($db, $consulta);
+
+if(!$id || !$resultado -> num_rows){ // num_rows valida si ha optenido valores
+    header('location: /bienes_raices/');
+}
+$propiedad = mysqli_fetch_assoc($resultado);
+
 ?>
 
 
-    <main class="contenedor seccion contenido-centrado">
-        <h1>Lake House</h1>
 
-        <picture>
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <source srcset="build/img/destacada.jpg" type="image/jpeg">
-            <img loading="lazy" src="/build/img/destacada.jpg" alt="Destacada">
+    <main class="contenedor seccion contenido-centrado">
+        <h1><?php echo $propiedad['titulo'] ?></h1>
+            <img src="/bienes_raices/imagenes/<?php echo $propiedad['imagen'] ?>" alt="Destacada">
         </picture>
 
         <div class="resumen-propiedad">
-            <p class="precio">$150,000.00</p>
+            <p class="precio">$<?php echo $propiedad['precio'] ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
-                    <img class="icono-dark-mode"  loading="lazy" src="/build/img/icono_wc.svg" alt="Icono">
-                    <p>2</p>
+                    <img class="icono-dark-mode"  loading="lazy" src="build/img/icono_wc.svg" alt="Icono">
+                    <p><?php echo $propiedad['wc'] ?></p>
                 </li>
 
                 <li>
-                    <img class="icono-dark-mode"  loading="lazy" src="/build/img/icono_estacionamiento.svg" alt="Icono">
-                    <p>3</p>
+                    <img class="icono-dark-mode"  loading="lazy" src="build/img/icono_estacionamiento.svg" alt="Icono">
+                    <p><?php echo $propiedad['estacionamiento'] ?></p>
                 </li>
 
                 <li>
-                    <img class="icono-dark-mode"  loading="lazy" src="/build/img/icono_dormitorio.svg" alt="Icono">
-                    <p>3</p>
+                    <img class="icono-dark-mode"  loading="lazy" src="build/img/icono_dormitorio.svg" alt="Icono">
+                    <p><?php echo $propiedad['habitaciones'] ?></p>
                 </li>
             </ul>
 
-            <p class="parrafo">Lorem ipsum dolor sit, amet consectetur adipisicing elit. At repellendus nulla tempora sint accusamus enim amet 
-                dolorum, ducimus odit possimus ipsa doloremque debitis hic a repellat perferendis error soluta quos.
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At repellendus nulla tempora sint accusamus enim amet 
-                dolorum, ducimus odit possimus ipsa doloremque debitis hic a repellat perferendis error soluta quos. 
-            </p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At repellendus nulla tempora sint accusamus enim amet 
-                dolorum, ducimus odit possimus ipsa doloremque debitis hic a repellat perferendis error soluta quos. 
-            </p>
+            <p class="parrafo"><?php echo $propiedad['descripcion'] ?> </p>
+            
 
         </div>
 
     </main>
 
     <?php
+    mysqli_close($db);
 incluirTemplate('footer');
 ?>
---
+++ includes/templates/anuncios.php
@@ -0,0 +1,52 @@
+<?php
+//Importar la conexion
+require 'includes/config/database.php';
+$db = conectarBD();
+
+//Consulta
+    $query = 'SELECT * from propiedades LIMIT '. $limite;
+
+    //Obtener resultados 
+    $resultado = mysqli_query($db, $query);
+
+
+?>
+<div class="contenedor-anuncios">
+<?php while($propiedad = mysqli_fetch_assoc($resultado)) : ?>
+<div class="anuncio">
+ 
+        <img loading="lazy" src="/bienes_raices/imagenes/<?php echo $propiedad['imagen']?>" alt="anuncio">
+
+
+    <div class="contenido-anuncio">
+        <h3><?php echo $propiedad['titulo']?></h3>
+        <p><?php echo $propiedad['descripcion']?></p>
+        <p class="precio">$<?php echo $propiedad['precio']?></p>
+
+        <ul class="iconos-caracteristicas">
+
+            <li>
+                <img class="icono-dark-mode"  src="build/img/icono_wc.svg" alt="icono wc" loading="lazy">
+                <p><?php echo $propiedad['wc']?></p>
+            </li>
+
+            <li>
+                <img class="icono-dark-mode"  src="build/img/icono_estacionamiento.svg" alt="icono wc" loading="lazy">
+                <p><?php echo $propiedad['estacionamiento']?></p>
+            </li>
+
+            <li>
+                <img class="icono-dark-mode"  src="build/img/icono_dormitorio.svg" alt="icono wc" loading="lazy">
+                <p><?php echo $propiedad['habitaciones']?></p>
+            </li>
+        </ul>
+
+        <a href="anuncio.php?id=<?php echo $propiedad['id']?>" class="boton boton-amarillo-block">
+            See Property
+        </a>
+    </div>
+</div>  <!-- Finaliza anuncio-->
+
+<?php endwhile; ?>
+
+</div>
